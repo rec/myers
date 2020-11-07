b74ca5f04238c46729914219bb47a1a612b3a29a
Tom Ritchford <tom@swirly.com>
2020-11-07 12:19:45 +0100

    Update Documentation

--- myers.py
+++ myers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """
 Myers diff of two lists or files
+---------------------------------
 
 Inspired by
 https://gist.github.com/adamnew123456/37923cf53f51d6b9af32a539cdfa7cc4
@@ -26,22 +27,22 @@ def diff(a, b, context=None, format=None):
     Return the Myers diff of two lists.
 
     The result is a list of (action, line) pairs,
-    where ``action`` is one of ``'kiro'``, for keep,
-    insert, remove, or omit.
+    where ``action`` is one of ``KEEP``, ``INSERT``, ``REMOVE``, or ``OMIT``
+    (which happen to be the characters k, i, r, and o).
 
     ARGUMENTS:
-       a, b:
+       ``a``, ``b``:
          The two files to compare
 
-       context:
+       ``context``:
          How many lines of context to keep between blocks of changes?
          ``None``, the default, means keep all unchanged lines.
-         ``0`` means throw away all the unchanged lines
+         ``0`` means throw away all the unchanged lines.
 
-       format: if a dictionary, it is used to format
+       ``format``:
+         If a dictionary, it is used to format
          each diff entry.  If true, the default dictionary above is
          used to format the diff entries.  Otherwise they are returned as-is.
-
     """
     diff = _myers(a, b)
 
